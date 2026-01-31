5d99837c624e14559f4307abeba97a4cd67823a5
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-01-31 20:59:26 +0500

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,11 @@
+Ali_Birthday = "23/1/2001"
+Abdulrehman_Birthday = "12/1/2013"
+Abdullah_Birthday = "24/8/2004"
+Maryam_Birthday = "14/6/2006"
+Ahmed_Birthday = "1/4/2003"
+
+print(Ali_Birthday, "Happy birthday!")
+print(Abdulrehman_Birthday, "Happy birthday!")
+print(Abdullah_Birthday, "Happy birthday!")
+print(Maryam_Birthday, "Happy birthday!")
+print(Ahmed_Birthday, "Happy birthday!")
