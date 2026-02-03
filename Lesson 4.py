7ae6e5d60bc8b7c00ae7d9e2dc4d3c3586a62c11
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-03 20:04:31 +0500

    Add files via upload

---
+++ Lesson 4.py
@@ -0,0 +1,32 @@
+#f = 9.8
+#i = int(f)
+#print(i)
+
+#str1 = "Hello"
+#str2 = "World"
+
+#print("str1 + str2", str1 + str2)
+#print("Result Of Concatination")
+
+#str1 = "string"
+
+#print(str1[0:2])
+#print(str1[3:6])
+
+#a = 6
+#print("Type of a", type(a))
+
+#b = 1.5
+#print("Type of b", type(b))
+
+#c = "Value"
+#print("Type of c", type(c))
+
+#d = False
+#print("Type of d", type(d))
+
+text = str(input("Enter a string"))
+revText = text[::-1]
+text = revText
+print("Your Reversed Text is:")
+print(text)
