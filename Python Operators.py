92808cc44a9c4a4b76d245a0e9f98736d282b373
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-07 20:49:16 +0500

    Add files via upload

---
+++ Python Operators.py
@@ -0,0 +1,30 @@
+"""a_tree = 91
+b_tree = 32
+c_tree = 90
+d_tree = 54
+e_tree = 15
+
+sum = a_tree + b_tree + c_tree + d_tree + e_tree
+
+solution = sum/5
+print(solution)"""
+
+"""math = 90
+science = 94
+english = 87
+hindi = 98
+
+sum = math+science+english+hindi
+solution = (sum/400)*100
+print("Percentage of Raj's score is", solution)"""
+
+Amount = 570
+
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+print("Notes in note 1 :", note_1)
+print("Notes in note 2 :", note_2)
+print("Notes in note 3 :", note_3)
+
