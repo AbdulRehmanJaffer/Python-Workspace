c562771fdcca44b75fc2318a018f1234b1289f98
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-10 20:26:42 +0500

    Add files via upload

---
+++ Submitting project conditional statements.py
@@ -0,0 +1,5 @@
+temperature = int(input("Enter the current temperature"))
+if temperature >= 21:
+    print("You should wear Normal soft light clothes because it is hot")
+else:
+    print("You should wear thick or a little warm clothes")
