fb5999d3d5052d83e85f8901aa25b30e740d300d
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-10 20:11:44 +0500

    Add files via upload

---
+++ Conditional statements and calculators.py
@@ -0,0 +1,34 @@
+#activity 1
+"""raining = input("Is it raining")
+
+if raining == "True":
+    print("Use an umbrella")
+
+else:
+    print("Wear a hat")"""
+#activity 2
+"""num = int(input("Choose a number"))
+if num >= 0:
+    print(num,"It is a posistive number")
+
+elif num <= -1:
+    print(num,"It is a negative number")"""
+    
+#Activity #
+"""O_P = int(input("Choose a value"))
+S_P = int(input("Choose a value"))
+
+Result = S_P - O_P
+
+if Result >= 0:
+    print(Result,"It is Profit!")
+
+else:
+    print(Result,"It is loss:(")"""
+
+num1 = int(input("Choose a number!"))
+
+if num1%2 == 0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
