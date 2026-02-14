d929cf974888913f50cec5c89d2bd74fb249927e
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-14 21:51:39 +0500

    Add files via upload
    I did this one without help.Took me very long.

---
+++ Conditional statement alphabets.py
@@ -0,0 +1,160 @@
+character1 = "A"
+character2 = "B"
+character3 = "C"
+character4 = "D"
+character5 = "E"
+character6 = "F"
+character7 = "G"
+character8 = "H"
+character9 = "I"
+character10 = "J"
+character11 = "K"
+character12 = "L"
+character13 = "M"
+character14 = "N"
+character15 = "O"
+character16 = "P"
+character17 = "Q"
+character18 = "R"
+character19 = "S"
+character20 = "T"
+character21 = "U"
+character22 = "V"
+character23 = "W"
+character24 = "X"
+character25 = "Y"
+character26 = "Z"
+character27 = "a"
+character28= "b"
+character29= "c"
+character30= "d"
+character31= "e"
+character32= "f"
+character33= "g"
+character34= "h"
+character35= "i"
+character36 = "j"
+character37 = "k"
+character38 = "l"
+character39 = "m"
+character40 = "n"
+character41 = "o"
+character42 = "p"
+character43 = "q"
+character44 = "r"
+character45 = "s"
+character46 = "t"
+character47 = "u"
+character48 = "v"
+character49 = "w"
+character50 = "x"
+character51 = "y"
+character52 = "z"
+
+user_input = input("Enter a character")
+if user_input == character1:
+    print("It is a part of the alphabet")
+elif user_input == character2:
+    print("It is a part of the alphabet")
+elif user_input == character3:
+    print("It is a part of the alphabet")
+elif user_input == character4:
+    print("It is a part of the alphabet")
+elif user_input == character5:
+    print("It is a part of the alphabet")
+elif user_input == character6:
+    print("It is a part of the alphabet")
+elif user_input == character7:
+    print("It is a part of the alphabet")
+elif user_input == character8:
+    print("It is a part of the alphabet")
+elif user_input == character9:
+    print("It is a part of the alphabet")
+elif user_input == character10:
+    print("It is a part of the alphabet")
+elif user_input == character11:
+    print("It is a part of the alphabet")
+elif user_input == character12:
+    print("It is a part of the alphabet")
+elif user_input == character13:
+    print("It is a part of the alphabet")
+elif user_input == character14:
+    print("It is a part of the alphabet")
+elif user_input == character15:
+    print("It is a part of the alphabet")
+elif user_input == character16:
+    print("It is a part of the alphabet")
+elif user_input == character17:
+    print("It is a part of the alphabet")
+elif user_input == character18:
+    print("It is a part of the alphabet")
+elif user_input == character19:
+    print("It is a part of the alphabet")
+elif user_input == character20:
+    print("It is a part of the alphabet")
+elif user_input == character21:
+    print("It is a part of the alphabet")
+elif user_input == character22:
+    print("It is a part of the alphabet")
+elif user_input == character23:
+    print("It is a part of the alphabet")
+elif user_input == character24:
+    print("It is a part of the alphabet")
+elif user_input == character25:
+    print("It is a part of the alphabet")
+elif user_input == character26:
+    print("It is a part of the alphabet")
+if user_input == character27:
+    print("It is a part of the alphabet")
+elif user_input == character28:
+    print("It is a part of the alphabet")
+elif user_input == character29:
+    print("It is a part of the alphabet")
+elif user_input == character30:
+    print("It is a part of the alphabet")
+elif user_input == character31:
+    print("It is a part of the alphabet")
+elif user_input == character32:
+    print("It is a part of the alphabet")
+elif user_input == character33:
+    print("It is a part of the alphabet")
+elif user_input == character34:
+    print("It is a part of the alphabet")
+elif user_input == character35:
+    print("It is a part of the alphabet")
+elif user_input == character36:
+    print("It is a part of the alphabet")
+elif user_input == character37:
+    print("It is a part of the alphabet")
+elif user_input == character38:
+    print("It is a part of the alphabet")
+elif user_input == character39:
+    print("It is a part of the alphabet")
+elif user_input == character40:
+    print("It is a part of the alphabet")
+elif user_input == character41:
+    print("It is a part of the alphabet")
+elif user_input == character42:
+    print("It is a part of the alphabet")
+elif user_input == character43:
+    print("It is a part of the alphabet")
+elif user_input == character44:
+    print("It is a part of the alphabet")
+elif user_input == character45:
+    print("It is a part of the alphabet")
+elif user_input == character46:
+    print("It is a part of the alphabet")
+elif user_input == character47:
+    print("It is a part of the alphabet")
+elif user_input == character48:
+    print("It is a part of the alphabet")
+elif user_input == character39:
+    print("It is a part of the alphabet")
+elif user_input == character50:
+    print("It is a part of the alphabet")
+elif user_input == character51:
+    print("It is a part of the alphabet")
+elif user_input == character52:
+    print("It is a part of the alphabet")
+else:
+    print("It is another chatacter")
