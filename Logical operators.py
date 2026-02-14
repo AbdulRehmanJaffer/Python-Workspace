980ca6ccb4d1bb94111779c31e661edd5e4441e6
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-14 21:03:49 +0500

    Add files via upload

---
+++ Logical operators.py
@@ -0,0 +1,49 @@
+#activity 1
+"""age = int(input("Enter your age"))
+height = int(input("Enter your height"))
+
+if age>12 or height>5:
+    print("You are ready for the ride")
+else:
+    print("You are not tall or old enough")"""
+#activity 2
+"""weight = float(input("Enter your weight in kg:"))
+height = float(input("Enter your height in m:"))
+
+BMI = weight/(height**2)
+print("Your bmi is:", BMI)
+
+if BMI <= 18.4:
+    print("You are underweight")
+elif BMI <= 24.9:
+    print("You are healthy")
+elif BMI <= 29.9:
+    print("Your are overweight")
+elif BMI <= 34.9:
+    print("Your are severely overweight")
+elif BMI <= 39.9:
+    print("Your are obese")
+else:
+    print("Your are severly obese")"""
+#activity 3
+"""a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value as true")
+else:
+    print("Atleast oen number has  value as false")
+a = 10
+b = -10
+c = 0
+
+if a>0 or b>0:
+    print("Either of the numbers are greater than zero")
+else:
+    print("No number is greater than 0")
+
+if b>0 or c>0:
+    print("Either number is greater than 0")
+else:
+    print("No number is greater than 0")"""
