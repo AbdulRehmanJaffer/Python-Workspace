0069daeda2acb5f30f177a9ff68b5897633d0ac5
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-14 21:25:01 +0500

    Add files via upload
    I took help from my father because I did not know about the "in" command.

---
+++ Python operators alphabet comparison submission.py
@@ -0,0 +1,12 @@
+characters = [
+    "A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h",
+    "I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p",
+    "Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x",
+    "Y","y","Z","z"
+]
+
+input = input("Enter a character")
+if input in characters:
+    print("These are Letters of the alphabet")
+else:
+    print("These are other characters")
