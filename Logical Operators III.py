3544547c13cd3bb1f8b3217e835c2510f184f266
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-02-17 20:11:08 +0500

    Add files via upload

---
+++ Logical Operators III.py
@@ -0,0 +1,19 @@
+
+"""#activity 3
+a = "apple"
+b = ["grapes","apple", "banana" ]
+
+if a in b:
+    print("Found")
+else:
+    print("Not found")"""
+
+a = 5
+b = 3
+
+#print(a & b)
+#print(a | b)
+#print(a ^ b)
+#print(~b)
+#print(a >> b)
+print(a << b)
