977b5c4fc8306aeb14b68c78779ee16ce57c6afa
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-03-01 12:16:33 +0500

    Add files via upload

---
+++ Age checker.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age:"))
+
+if age >= 10:
+    if age <= 20:
+        print("Your age is in between 10 or 20")
+    else:
+        print("Your age is NOT between 10 and 20.")
