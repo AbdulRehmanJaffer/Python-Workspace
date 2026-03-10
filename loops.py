be510011ea85c66e37cb8af525606d8ff205b48a
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-03-10 20:14:42 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,27 @@
+"""word = "hello"
+for i in word:
+    print(i)"""
+
+"""for i in range(10):
+    print(i)"""
+
+#activity 1
+"""n = int(input("Enter a number: "))
+sum = 0
+
+for i in range(n):
+    print(i)
+    sum = sum + i
+    print(sum)"""
+#activity 2
+"""word = input("Enter a word")
+string2 = ""
+
+for i in word:
+    print(i)
+    string2 = i + string2
+    print(string2)"""
+#activity 3
+
+for i in range(10,1,-1):
+    print(i)
