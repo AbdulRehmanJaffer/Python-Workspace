dbb9272d1189a5208889826c557e67132670b5ec
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-03-14 20:44:18 +0500

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,32 @@
+"""i = 1
+while(i < 6):
+    print(i)
+    i += 1"""
+#activity 1
+"""n = int(input("Enter a random number: "))
+sum = 0
+
+i = 1
+while(i <= n):
+    sum = sum + i
+    i += 1
+    print(sum)"""
+#activity 2
+"""i = 0
+while(i <= 0):
+    print("I WILL RUN FOREVER")"""
+#activity 3
+"""num = int(input("Enter a number: "))
+sum = 0
+
+temp = num
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print("It is an armstrong numer")
+else:
+    print("It is not an armstrong number")"""
