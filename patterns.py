d94cb87bd9a1168e182f4ec860bc5c5d81d18ddd
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-03-24 20:11:38 +0500

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,35 @@
+"""for i in range(3):
+    for x in range(5):
+        print(x, end=" ")
+    print()"""
+#activity 1
+"""print("Half pyramid pattern of stars(*)")
+n = int(input("Enter the number of rows"))
+
+for i in range(n):
+    for j in range(i+1):
+        print("* ", end=" ")
+    print()"""
+#activity 2
+"""print("Full Pyramid:")
+n = int(input("enter the number of rows: "))
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+    for j in range(i+1):
+        print("*  ", end=" ")
+        
+    print()"""
+#activity 3
+"""print("Full Pyramid:")
+n = int(input("enter the number of rows: "))
+
+for i in range(n):
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(n + i):
+        print("*  ", end=" ")
+
+        
+    print()"""
