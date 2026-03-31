5535ce47bc40996b84dcea907ed5fd7ecfb2d67d
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-03-31 20:56:16 +0500

    Add files via upload

---
+++ circumfrence of circle.py
@@ -0,0 +1,12 @@
+#Circumfrence of a circle
+#p = pi
+#r = radius
+#circumfrence of a circle is: pi(r)*2
+
+def circumfrence(p,r):
+    p = 3.141
+    r = int(input("Enter a value"))
+    print(2*p*r)
+
+circumfrence(3.141, 50)
+    
