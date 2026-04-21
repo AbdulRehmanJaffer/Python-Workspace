20cf16fa5650ebc66265605d30305ce70b1517b2
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-04-21 20:09:22 +0500

    Add files via upload

---
+++ Handlers.py
@@ -0,0 +1,33 @@
+#activity 2
+"""try:
+    num1, num2 = eval(input("Enter 2 numbers, separarted by comma "))
+
+    result = num1 / num2
+    print("Result is,", result)
+
+except ZeroDivisionError:
+    print("Division by zero is erorr!! !")
+
+except SyntaxError:
+    print("Commas are not put properly try doing it like, 1, 2")
+
+except:
+    print("Wrong Input!! !")
+
+else:
+    print("No exceptions")
+
+finally:
+    print("Execution Finished!! !")"""
+
+#activity 3
+"""valid = False
+while not valid:
+    try:
+        n = int(input("Enter a number: "))
+        while n%2 == 0:
+            print("bye")
+            valid = True
+
+    except ValueError:
+        print("Invalid")"""
