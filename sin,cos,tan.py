6a931339be9556efc43ee6f4c2922737cf3b8e90
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-04-25 21:12:32 +0500

    Add files via upload

---
+++ sin,cos,tan.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+print("Here are the values of random numbers using sin, cos and tan")
+
+x = random.randint(0,50)
+
+print("The number is: ",x)
+print("The sin value of " + str(x) + " is " + str(math.sin(x)))
+
+y = random.randint(0,10)
+
+print("The number is: ",x)
+print("The cos value of " + str(y) + " is " + str(math.cos(y)))
+
+z = random.randint(0,30)
+
+print("THe number is: ",x)
+print("The cos value of " + str(z) + " is " + str(math.tan(z)))
