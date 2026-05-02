b7406cefe5146e9fdf38f86044762d0db241c5da
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-05-02 21:10:41 +0500

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,61 @@
+"""x = ["P", "Y", "T", "H", "O", "N"]
+print(x[0:4])
+print(x[-2:])"""
+
+"""my_list = [6, 7, "Billie", 17, "Ironman"]
+
+for i in my_list:
+    print(i)"""
+
+"""-list_1 = [6, 7, 6, 7]
+list_2 = ['Tom', "Billie", "batman"]
+
+list_1.extend(list_2)
+print(list_1)"""
+
+"""numbers = [40, 10, 30, 20]
+numbers.sort()
+print(numbers)"""
+
+"""nested_list = [[2, 4, 6, 8, 10],[1, 3, 5, 7, 9],[1, 2, 3, 4, 5]]
+
+print("Accessing the third element of the second list :", nested_list[2][4])
+
+for i in nested_list:
+    print("list",i,"elements")
+    for j in i:
+        print(j)"""
+
+#activity 1
+"""empty_list = []
+print()
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triples = [1, 2, 3]*3
+print(triples)
+
+aList = [100, 200, 300, 400, 500]
+aList.reverse()
+print(aList)"""
+
+#activity 2
+"""my_list = [4, 5, 1, 2, 9, 7, 10, 8]
+print("Original list:-", my_list)
+
+count = 0
+
+for i in my_list:
+    count += 1
+
+avg = count/len(my_list)
+print(avg)
+
+print(count)
+
+y = max(my_list)
+print(y)
+
+z = min(my_list)
+print(z)"""
