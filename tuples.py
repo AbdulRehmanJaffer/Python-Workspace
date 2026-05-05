365ba944e62f3e28fac59e64e023b7db905cd5e7
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-05-05 20:18:28 +0500

    tuples.py

--- tuples.py
+++ tuples.py
@@ -10,3 +10,24 @@ print(tuple1)
 nest_tup = (1, 2,("P", "Y"), [4, 5, "Python", 5, 6])
 
 print(nest_tup[3][2])
+
+#activity 1
+
+weather = 0
+sunny = 0
+rainy = 0
+
+weather = tuple(int(input()) for i in range(7))
+
+for i in range(7):
+
+    weather[i]
+    if weather[i] == 0:
+        sunny += 1
+    else:
+        rainy += 1
+
+if sunny > rainy:
+    print("It was good weather")
+else:
+    print("It was rainy weather")
