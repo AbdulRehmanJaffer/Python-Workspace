c01ac83c1e3c9228e7631f89c24d75b854e5e2c2
AbdulRehmanJaffer <13arsultan@gmail.com>
2026-08-20 21:39:25 +0500

    Add files via upload

--- SentimentAI.py
+++ SentimentAI.py
@@ -1,34 +1,67 @@
 import colorama
-from colorama import fore, style
-from textblob import Textblob
+from colorama import Fore, Style
+from textblob import TextBlob
 
 colorama.init()
 
-print(f"{fore.CYAN} Welcome to Sentiment AI! {Style.RESET_ALL}")
+print(f"{Fore.CYAN} Welcome to Sentiment AI! {Style.RESET_ALL}")
 
-user_name = input(f"{fore.MAGENTA} Please enter your name: {Style.rESET_ALL}")
+user_name = input(f"{Fore.MAGENTA} Please enter your name: {Style.RESET_ALL}")
 if not user_name:
     user_name = "Mystery Agent"
 
 conversation_history = []
 
-print(f"\n{fore.CYAN}Hello, Agent {user_name}!")
+print(f"\n{Fore.CYAN}Hello, Agent {user_name}!")
 print(f"Type a sentence and I will analyza you sentence with Textblob and show you the sentiment. ")
-print(f"Type {fore.YELLOW}reset{fore.CYAN}, {fore.YELLOW}History{fore.CYAN}, "
-      f" {fore.YELLOW}exit{fore.CYAN} to quit.{Style.RESET_ALL}\n")
+print(f"Type {Fore.YELLOW}reset{Fore.CYAN}, {Fore.YELLOW}History{Fore.CYAN}, "
+      f" {Fore.YELLOW}exit{Fore.CYAN} to quit.{Style.RESET_ALL}\n")
 while True:
-    user_input = input(f"{fore.GREEN}>> {Style.RESET_ALL}").strip()
+    user_input = input(f"{Fore.GREEN}>> {Style.RESET_ALL}").strip()
 
     if not user_input:
-        print(f"{fore.RED} Please enter some text or a valid command {Style.RESET_ALL}")
+        print(f"{Fore.RED} Please enter some text or a valid command {Style.RESET_ALL}")
         continue
 
     if user_input.lower() == "exit":
-        print(f"\n{fore.BLUE} Exiting Sentiment AI. farewell, Agent {user_name} ! {Style.RESET_ALL}")
+        print(f"\n{Fore.BLUE} Exiting Sentiment AI. farewell, Agent {user_name} ! {Style.RESET_ALL}")
         break
 
     elif user_input.lower() == "reset":
         conversation_history.clear()
-        print(f"{fore.CYAN} All conversation history cleared!{Style.RESET_ALL}")
+        print(f"{Fore.CYAN} All conversation history cleared!{Style.RESET_ALL}")
         continue
 
+    elif user_input.lower() == "history":
+        if not conversation_history:
+            print(f"{Fore.YELLOW} No conversation history found. {Style.RESET_ALL}")       
+        else:
+            print(f"{Fore.CYAN} Conversation History: {Style.RESET_ALL}")
+            for idx, (text, polarity, sentiment_type) in enumerate(conversation_history, start=1):
+                if sentiment_type == "Positive":
+                    color = Fore.GREEN
+                elif sentiment_type == "Negative":
+                    color = Fore.RED
+                else:
+                    color = Fore.YELLOW
+
+                print(f"{idx}. {color} {text}"f"Polarity: {polarity:.2f}, {sentiment_type}{Style.RESET_ALL}")
+
+        continue
+
+    polarity = TextBlob(user_input).sentiment.polarity
+    if polarity > 0.25:
+        sentiment_type = "Positive"
+        color = Fore.GREEN
+    elif polarity < -0.25:
+        sentiment_type = "Negative"
+        color = Fore.RED 
+    else:
+        sentiment_type = Fore.YELLOW 
+        sentiment_type = "Neutral"
+
+    conversation_history.append((user_input, polarity, sentiment_type))
+
+    print(f"{color} {sentiment_type} sentiment detected! "f"Polaraity: {polarity:.2f}")
+
+    
